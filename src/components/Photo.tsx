25075de9a159fc24d00a9e9a12c4edfcfcac2d10
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-09-06 23:58:54 +0100

    updated post contact

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import image from "../assets/images/my-pic-removebg-preview.png";
 import { motion } from "framer-motion";
@@ -12,35 +12,36 @@ export default function Photo() {
         animate={{
           opacity: 1,
           transition: { delay: 2, duration: 0.4, ease: "easeIn" },
-        }}>
+        }}
+        className="relative flex justify-center items-center"
+      >
+        {/* image container */}
         <motion.div
-          className="absolute w-[220px] h-[220px] md:w-[238px] md:h-[238px] lg:w-[280px] lg:h-[300px] flex justify-center items-center"
-          style={{
-            top: "50%",
-            left: "43.6%",
-            transform: "translate(-50%, -50%)",
-          }}
+          className="absolute flex justify-center items-center rounded-full overflow-hidden
+            w-[220px] h-[220px] md:w-[238px] md:h-[238px] lg:w-[280px] lg:h-[300px]"
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
-          }}>
+          }}
+        >
           <Image
             src={image}
             alt="me"
-            className="object-cover w-full h-full rounded-full"
+            className="object-cover w-full h-full"
           />
         </motion.div>
 
-        {/* circle */}
+        {/* animated circle (fixed center) */}
         <motion.svg
           className="w-[270px] h-[270px] md:w-[310px] md:h-[310px] lg:w-[378px] lg:h-[378px]"
           fill="transparent"
-          viewBox="0 0 586 506"
-          xmlns="http://www.w3.org/2000/svg">
+          viewBox="0 0 600 600"   // square canvas
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <motion.circle
-            cx="253"
-            cy="253"
+            cx="300"   // center horizontally
+            cy="300"   // center vertically
             r="250"
             stroke="#00ff99"
             strokeWidth="4"
@@ -48,7 +49,11 @@ export default function Photo() {
             strokeLinejoin="round"
             initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              strokeDasharray: [
+                "15 120 25 25",
+                "16 25 92 72",
+                "4 250 22 22",
+              ],
               rotate: [120, 360],
             }}
             transition={{ duration: 20, repeat: Infinity }}
